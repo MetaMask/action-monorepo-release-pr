724de5d327bce5e56b3b7992cdece04787171e48
Erik Marks <rekmarks@protonmail.com>
2021-04-20 20:49:40 -0700

    Fix Action PR creation script

--- src/create-release-pr.sh
+++ src/create-release-pr.sh
@@ -12,21 +12,24 @@ if [[ -z $NEW_VERSION ]]; then
   exit 1
 fi
 
-BRANCH_NAME="release-v${NEW_VERSION}"
+RELEASE_BRANCH_NAME="release-v${NEW_VERSION}"
 RELEASE_BODY="This is the release candidate for version ${NEW_VERSION}."
 
 git config user.name github-actions
 git config user.email github-actions@github.com
 
-if ! (git add . && git commit -m "${NEW_VERSION}" && git push);
+git checkout -b "${RELEASE_BRANCH_NAME}"
+
+if ! (git add . && git commit -m "${NEW_VERSION}");
 then
     echo "Error: No changes detected."
     exit 1
 fi
 
-hub pull-request \
-    --draft \
-    --message "${NEW_VERSION} RC" \
-    --message "${RELEASE_BODY}" \
-    --base "main" \
-    --head "${BRANCH_NAME}";
+git push --set-upstream origin "${RELEASE_BRANCH_NAME}"
+
+gh pr create \
+  --draft \
+  --title "${NEW_VERSION} RC" \
+  --body "${RELEASE_BODY}" \
+  --head "${RELEASE_BRANCH_NAME}";
